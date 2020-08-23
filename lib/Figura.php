e46a29ecb0b0605fab6fdc335af4c8e9e4e980e1
Franco Stramana <franco.stramana@gmail.com>
2020-08-23 09:40:28 -0300

    refactoring para ejercicio nuevo

--- lib/Figura.php
+++ lib/Figura.php
@@ -2,6 +2,18 @@
 <?php
     abstract class Figura
     {
+        protected $id;
+
+        public function getId() 
+        {
+            return $this->id;
+        }
+
+        public function setId($id) 
+        {
+            $this->id = $id;
+        }
+        
         public abstract function getName();
 
         public abstract function getArea();
